c0bcd7476720ca09cc0bc3967b7c635447d4723f
Przemek <przemo247@outlook.com>
2021-06-30 09:54:19 +0200

    Change Blockchain class property newTransactions to pendingTransactions, modify createNewTransaction method to push newTransaction to pendingTransactions array and return index of the block the transaction will be added to

--- blockchain.js
+++ blockchain.js
@@ -1,19 +1,19 @@
 class Blockchain {
   constructor() {
     this.chain = [];
-    this.newTransactions = [];
+    this.pendingTransactions = [];
   }
 
   createNewBlock(nonce, previousBlockHash, hash) {
     const newBlock = {
       index: this.chain.length + 1,
       timestamp: Date.now(),
-      transactions: this.newTransactions,
+      transactions: this.pendingTransactions,
       nonce,
       hash,
       previousBlockHash,
     };
-    this.newTransactions = [];
+    this.pendingTransactions = [];
     this.chain.push(newBlock);
     return newBlock;
   }
@@ -26,6 +26,8 @@ class Blockchain {
       sender,
       recipient,
     };
+    this.pendingTransactions.push(newTransaction);
+    return this.getLastBlock["index"] + 1;
   }
 }
 
--- blockchain.test.js
+++ blockchain.test.js
@@ -2,5 +2,5 @@ const Blockchain = require("./blockchain");
 
 test("Empty Blockchain class instance", () => {
   const data = new Blockchain();
-  expect(data).toEqual({ chain: [], newTransactions: [] });
+  expect(data).toEqual({ chain: [], pendingTransactions: [] });
 });
