12a2d1ef15a61321dcc797c44cd50ce925e2f15c
Przemek <przemo247@outlook.com>
2021-11-06 12:46:26 +0100

    Make mine and transaction endpoints work

--- dev/api.js
+++ dev/api.js
@@ -1,19 +1,44 @@
 const express = require("express");
 const app = express();
+const Blockchain = require("./blockchain");
+
+const bitcoin = new Blockchain();
 
 app.use(express.json());
 app.use(express.urlencoded());
 
 app.get("/blockchain", function (req, res) {
-  res.send("Hello World");
+  res.send(bitcoin);
 });
 
 app.post("/transaction", function (req, res) {
-  console.log(req.body);
-  res.send(`The amount of the transaction is ${req.body.amount} bitcoin.`);
+  const blockIndex = bitcoin.createNewTransaction(
+    req.body.amount,
+    req.body.sender,
+    req.body.recipient
+  );
+  res.json({ note: `Transaction will be added in block ${blockIndex}` });
 });
 
-app.get("/mine", function (req, res) {});
+app.get("/mine", function (req, res) {
+  const lastBlock = bitcoin.getLastBlock();
+  const previousBlockHash = lastBlock["hash"];
+  const currentBlockData = {
+    transactions: bitcoin.pendingTransactions,
+    index: lastBlock["index"] + 1,
+  };
+  const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData);
+  const blockHash = bitcoin.hashBlock(
+    previousBlockHash,
+    currentBlockData,
+    nonce
+  );
+  const newBlock = bitcoin.createNewBlock(nonce, previousBlockHash, blockHash);
+  res.json({
+    note: "New block mined sucessfully",
+    block: newBlock,
+  });
+});
 
 app.listen(3000, function () {
   console.log("Listening on port 3000");
